7687b80bce9181f2a4bec9517d3ca066c4cdb981
yash goyal <yashgoyal7661@gmail.com>
2026-03-24 20:15:10 +0530

    user

---
+++ Job_ui/src/Components/Card.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {Bookmark} from "lucide-react";
+const Card = (props) => {
+  
+  return (
+      <div className="card">
+      <div>
+        <div className="top">
+        <img src={props.brandLogo} alt={props.company + " icon"} />
+        <button>Save <Bookmark size={10}/></button>
+      </div>
+      <div className="center">
+<h3>{props.company} <span>{props.posted}</span></h3>
+<h2>{props.post}</h2>
+    <div className="tag">
+      <h4>{props.tag1}</h4>
+      <h4>{props.tag2}</h4>
+    </div>
+      </div>
+      </div>
+      <div className="bottom">
+        <div>
+        <h3>{props.pay}</h3>
+        <p>{props.location}</p>
+
+        </div>
+        <button> Apply now </button>
+
+      </div>
+    </div>
+  )
+}
+
+export default Card
